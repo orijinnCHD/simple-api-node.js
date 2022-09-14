10e8dd0be298f1f17bf7bcaffb6f3637ce06acd7
orijinnCHD <reussir.startup@gmail.com>
2022-09-14 17:25:04 +0400

    init

---
+++ index.js
@@ -0,0 +1,10 @@
+
+// const express = require('express');
+// const app = express();
+
+
+// // créer le serveur
+// // paremetre au port 5500
+// //le domaine n'est pas definie dans en localhost
+// app.listen(5500);
+// console.log("Attente des requêtes au port 5500");
